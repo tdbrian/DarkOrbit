9a7325ac81119cd7f234d902fe6ee3dfbb19df0e
Thomas Brian <tbrian@appriver.com>
2018-01-13 15:18:02 -0600

    Setup table styling

--- DarkOrbit.WebApp/src/app/api-services/api-services-list/api-services-list.component.ts
+++ DarkOrbit.WebApp/src/app/api-services/api-services-list/api-services-list.component.ts
@@ -6,16 +6,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApiServicesListComponent implements OnInit {
 
-  rows = [
+  services = [
     { name: 'User Management', type: 'JSONAPI REST', description: 'Handles all user creation, querying, etc.' },
     { name: 'Domain Swaps', type: 'JSONAPI REST', description: 'KFC' },
     { name: 'Price List', type: 'JSONAPI REST', description: 'Burger King' },
-  ];
-
-  columns = [
-    { prop: 'name' },
-    { name: 'Description' },
-    { name: 'Type' }
   ];
 
   constructor() { }
