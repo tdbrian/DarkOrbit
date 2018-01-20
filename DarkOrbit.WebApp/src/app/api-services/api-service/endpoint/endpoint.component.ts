b5480a608c3a883ba4573c2ca31b8ad96746384e
Thomas Brian <tbrian@appriver.com>
2018-01-20 14:59:09 -0600

    Add setup endpoint method(s) buttons

--- DarkOrbit.WebApp/src/app/api-services/api-service/endpoint/endpoint.component.ts
+++ DarkOrbit.WebApp/src/app/api-services/api-service/endpoint/endpoint.component.ts
@@ -5,6 +5,7 @@ import { NotificationsService } from 'angular2-notifications';
 import { EndpointsService } from '../../../api/services/endpoints.service';
 import { MicroServicesService } from '../../../api/services/micro-services.service';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-endpoint',
@@ -12,6 +13,8 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./endpoint.component.scss']
 })
 export class EndpointComponent implements OnInit {
+  isLoading = true;
+  firstView = true;
   endpoints: EndpointEntity[];
   endpoint: EndpointEntity;
   service: MicroServiceEntity;
@@ -42,19 +45,32 @@ export class EndpointComponent implements OnInit {
     private notifications: NotificationsService,
     private endpointsSvc: EndpointsService,
     private servicesSvc: MicroServicesService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   async ngOnInit() {
-    this.route.params.subscribe( async ({ serviceId, endpointId }) => {
+    this.route.params.subscribe(async ({ serviceId, endpointId }) => {
       try {
         [this.endpoints, this.service] = await Promise.all([
           this.endpointsSvc.ApiEndpointsByServiceByServiceIdGet(serviceId).toPromise(),
           this.servicesSvc.ApiMicroServicesByIdGet(serviceId).toPromise()
         ]);
         this.endpoint = this.endpoints.find(endpoint => endpoint.id === endpointId);
+        if (this.endpoint === null) {
+          this.notifications.error('Error', 'Endpoint not found.');
+          this.router.navigateByUrl('../');
+        }
+        if (this.endpoint.endpointMethods.length > 0) {
+          this.firstView = true;
+        }
+        this.isLoading = false;
       } catch (error) {
         this.notifications.error('Error', 'Getting initial data');
       }
     });
+  }
+
+  newMethod() {
+
   }
 }
